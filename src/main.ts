883bb03b5cc5ff19d16c03ae0cf43f7dae0a5402
Chase Houske <chouske@ucsc.edu>
2023-10-10 20:10:39 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,15 @@ const gameName = "Chase's game";
 document.title = gameName;
 const button = document.createElement("button");
 const header = document.createElement("h1");
+const mydiv = document.createElement("div");
 header.innerHTML = gameName;
 button.innerHTML = "🍭";
+let counter: number = 0;
+mydiv.innerHTML = counter + " lollipops";
+button.addEventListener("click", function () {
+  counter++;
+  mydiv.innerHTML = counter + " lollipops";
+});
 app.append(header);
 app.append(button);
+app.append(mydiv);
