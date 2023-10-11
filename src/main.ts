df765147d6bff24721be727ff6c3aa5718e23fd2
Chase Houske <chouske@ucsc.edu>
2023-10-10 18:29:43 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Chase's game";
 
 document.title = gameName;
-
+const button = document.createElement("button");
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+button.innerHTML = "🍭";
 app.append(header);
+app.append(button);
